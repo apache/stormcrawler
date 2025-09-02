c0e3fe54b9b1f553815300cb5f133b84bd8b4fb3
Sigee <sigee15@gmail.com>
2025-09-02 15:49:22 +0200

    Improve MetadataFilter

--- core/src/main/java/org/apache/stormcrawler/filtering/metadata/MetadataFilter.java
+++ core/src/main/java/org/apache/stormcrawler/filtering/metadata/MetadataFilter.java
@@ -18,9 +18,10 @@ package org.apache.stormcrawler.filtering.metadata;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.net.URL;
-import java.util.LinkedList;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.function.Predicate;
 import org.apache.stormcrawler.Metadata;
 import org.apache.stormcrawler.filtering.URLFilter;
 import org.jetbrains.annotations.NotNull;
@@ -33,7 +34,7 @@ public class MetadataFilter extends URLFilter {
 
     private static final Logger LOG = LoggerFactory.getLogger(MetadataFilter.class);
 
-    private final LinkedList<String[]> mdFilters = new LinkedList<>();
+    private final ComplexFilter filters = new ComplexFilter();
 
     @Override
     public void configure(@NotNull Map<String, Object> stormConf, @NotNull JsonNode paramNode) {
@@ -42,8 +43,20 @@ public class MetadataFilter extends URLFilter {
             Entry<String, JsonNode> entry = iter.next();
             String key = entry.getKey();
             String value = entry.getValue().asText();
-            mdFilters.add(new String[] {key, value});
+            filters.addFilter(key, value);
         }
+    }
+
+    public void addFilter(String key, String value) {
+        filters.addFilter(key, value);
+    }
+
+    public void addFilter(ComplexFilter filter) {
+        filters.addFilter(filter);
+    }
+
+    public void setOperation(FilterOperation operation) {
+        filters.setOperation(operation);
     }
 
     @Override
@@ -52,19 +65,93 @@ public class MetadataFilter extends URLFilter {
         if (sourceMetadata == null) {
             return urlToFilter;
         }
-        // check whether any of the metadata can be found in the source
-        for (String[] kv : mdFilters) {
-            String[] vals = sourceMetadata.getValues(kv[0]);
-            if (vals == null) {
-                continue;
+        if (sourceMetadata.asMap().isEmpty()) {
+            return urlToFilter;
+        }
+        if (filters.filters.isEmpty()) {
+            return urlToFilter;
+        }
+
+        boolean shouldFilter =
+                recursiveFilter(filters.operation, filters, sourceMetadata, urlToFilter);
+        if (shouldFilter) {
+            return null;
+        }
+
+        return urlToFilter;
+    }
+
+    private static boolean recursiveFilter(
+            FilterOperation operation,
+            ComplexFilter complexFilter,
+            Metadata sourceMetadata,
+            String urlToFilter) {
+        if (operation == FilterOperation.OR) {
+            return complexFilter.filters.entrySet().stream()
+                    .anyMatch(getPredicate(sourceMetadata, urlToFilter));
+        } else if (operation == FilterOperation.AND) {
+            return complexFilter.filters.entrySet().stream()
+                    .allMatch(getPredicate(sourceMetadata, urlToFilter));
+        }
+        return false;
+    }
+
+    private static @NotNull Predicate<Entry<String, Object>> getPredicate(
+            Metadata sourceMetadata, String urlToFilter) {
+        return entrySet -> {
+            if (entrySet.getValue() instanceof ComplexFilter) {
+                return recursiveFilter(
+                        ((ComplexFilter) entrySet.getValue()).operation,
+                        (ComplexFilter) entrySet.getValue(),
+                        sourceMetadata,
+                        urlToFilter);
             }
+            String[] vals = sourceMetadata.getValues(entrySet.getKey());
+            if (vals == null) {
+                return false;
+            }
+
             for (String v : vals) {
-                if (v.equalsIgnoreCase(kv[1])) {
-                    LOG.debug("Filtering {} matching metadata {}:{}", urlToFilter, kv[0], kv[1]);
-                    return null;
+                if (entrySet.getValue() instanceof String) {
+                    if (v.equalsIgnoreCase((String) entrySet.getValue())) {
+                        LOG.debug(
+                                "Filtering {} matching metadata {}:{}",
+                                urlToFilter,
+                                entrySet.getKey(),
+                                entrySet.getValue());
+                        return true;
+                    }
                 }
             }
+
+            return false;
+        };
+    }
+
+    public static class ComplexFilter {
+        private final Map<String, Object> filters = new HashMap<>();
+        private FilterOperation operation = FilterOperation.OR;
+
+        public void setOperation(FilterOperation operation) {
+            this.operation = operation;
         }
-        return urlToFilter;
+
+        public void addFilter(String key, String value) {
+            filters.put(key, value);
+        }
+
+        public void addFilter(ComplexFilter filter) {
+            String key = "unique_key_for_complex_filtering_";
+            int counter = 1;
+            while (filters.containsKey(key + counter)) {
+                counter++;
+            }
+            filters.put(key + counter, filter);
+        }
+    }
+
+    public enum FilterOperation {
+        OR,
+        AND
     }
 }
