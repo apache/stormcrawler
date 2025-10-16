2b08e3516a74b4958bb02f4753d73f55cae7797e
Dávid Szigecsán <sigee@apache.org>
2025-10-17 00:12:01 +0200

    Add colon after TODO

--- core/src/main/java/org/apache/stormcrawler/bolt/FeedParserBolt.java
+++ core/src/main/java/org/apache/stormcrawler/bolt/FeedParserBolt.java
@@ -83,7 +83,7 @@ public class FeedParserBolt extends StatusEmitterBolt {
             if (sniffWhenNoMetadataKey) {
                 // uses mime-type
                 // won't work when servers return text/xml
-                // TODO use Tika instead?
+                // TODO: use Tika instead?
                 String ct = metadata.getFirstValue(HttpHeaders.CONTENT_TYPE, protocolMetadataPrefix);
                 if (ct != null && ct.contains("rss+xml")) {
                     isfeed = true;
