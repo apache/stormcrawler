d1cdec0456c7e3a7c1a56ecb37f3de9daa244700
Julien Nioche <julien@digitalpebble.com>
2026-03-30 10:58:07 +0100

    Remove unneeded import
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- external/urlfrontier/src/test/java/org/apache/stormcrawler/urlfrontier/StatusUpdaterBoltTest.java
+++ external/urlfrontier/src/test/java/org/apache/stormcrawler/urlfrontier/StatusUpdaterBoltTest.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.apache.storm.task.OutputCollector;
