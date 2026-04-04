55ff901244520588fae4b157905ba0f0937e35bf
Julien Nioche <julien@digitalpebble.com>
2026-04-04 15:24:57 +0100

    checkstyle fix (again - should go back to eating chocolate instead of working in my spare time)
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/WaitAckCacheTest.java
+++ external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/WaitAckCacheTest.java
@@ -20,6 +20,7 @@ package org.apache.stormcrawler.opensearch;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
