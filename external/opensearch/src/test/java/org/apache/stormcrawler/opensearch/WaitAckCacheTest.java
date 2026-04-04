7788d48c9645df8948b9ca50ab9d00929f4b09ff
Julien Nioche <julien@digitalpebble.com>
2026-04-04 15:21:01 +0100

    checkstyle fix
    Signed-off-by: Julien Nioche <julien@digitalpebble.com>

--- external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/WaitAckCacheTest.java
+++ external/opensearch/src/test/java/org/apache/stormcrawler/opensearch/WaitAckCacheTest.java
@@ -18,7 +18,9 @@
 package org.apache.stormcrawler.opensearch;
 
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
